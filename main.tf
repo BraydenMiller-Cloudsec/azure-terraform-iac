051f7b561208138833721969ecfee03793f5c53e
BraydenMiller-CloudSec <270777548+BraydenMiller-Cloudsec@users.noreply.github.com>
2026-04-14 22:27:09 +0000

    Update remote backend and fix resource group deletion

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ terraform {
   required_version = ">= 1.0"
 
   backend "azurerm" {
-    resource_group_name  = "terraform-infra-rg"
-    storage_account_name = "tfstatebrayden"
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "tfstatebrayden2"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
@@ -19,6 +19,9 @@ provider "azurerm" {
   features {
     key_vault {
       purge_soft_delete_on_destroy = true
+    }
+    resource_group {
+      prevent_deletion_if_contains_resources = false
     }
   }
 }
